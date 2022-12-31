ccc3b54edd68bd91d69a2b37a8448db1b9d3687e
ayabdi <abdulrahmanabdi98@gmail.com>
2022-12-31 20:20:13 +0300

    rename file

--- src/components/MessageInputBox.tsx
+++ src/components/MessageInputBox.tsx
@@ -4,7 +4,7 @@ interface MessageBoxProps {
   onSubmit: (message: string) => void;
 }
 
-export const MessageBox = (props: MessageBoxProps) => {
+export const MessageInputBox = (props: MessageBoxProps) => {
   const [message, setMessage] = React.useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -14,7 +14,7 @@ export const MessageBox = (props: MessageBoxProps) => {
   };
 
   return (
-    <div className="h-26 flex w-full flex-col rounded bg-[#36363B] px-3  py-2">
+    <div className="h-26 flex w-full flex-col rounded bg-[#36363B] px-3 py-2 mt-6">
       <div className="h-6 w-full">
         <img className="inline h-6" src="/icons/tag.svg" />
         <img className="ml-1.5 inline h-6" src="/icons/plus.svg" />
