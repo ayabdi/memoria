b0b36cdc613fbb9a971dfa6ba96692e40c35a382
ayabdi <abdulrahmanabdi98@gmail.com>
2023-01-04 22:32:12 +0300

    fixed schema

--- src/ui/MessageRow.tsx
+++ src/ui/MessageRow.tsx
@@ -1,21 +1,20 @@
 import { useSession } from "next-auth/react";
 import React from "react";
 import Avatar from "react-avatar";
-import { Message, TagsOnMessages, Tag } from "@prisma/client";
+import { Tag } from "@prisma/client";
 
-interface MessageRowProps {
-  message: Message & {
-    tags: (TagsOnMessages & {
-      tag: Tag;
-    })[];
-  };
+interface MessageRowProps{
+  text: string;
+  from: string;
+  createdAt: Date;
+  tags: Tag[];
   hideLabels?: boolean;
   className?: string;
 }
 
 export const MessageRow = (props: MessageRowProps) => {
-  const { message, className } = props;
-  const { text, from, createdAt, tags } = message;
+  const { text, from, createdAt, tags, className } = props;
+
   const formattedDate = new Date(createdAt).toLocaleDateString("en-US", {
     hour: "numeric",
     minute: "numeric",
@@ -37,14 +36,14 @@ export const MessageRow = (props: MessageRowProps) => {
           <div className="flex">
             {tags.map((tag) => (
               <div
-                className={`my-1 mr-1.5 w-max rounded-2xl border px-3.5 py-0.5 text-sm text-white`}
+                className={`my-1 mr-1.5 -ml-1 w-max rounded-2xl border px-2.5 py-[1px] text-[13px] text-white`}
                 style={{
-                  backgroundColor: tag.tag.color.replace("1)", "0.2)"),
+                  backgroundColor: tag.color.replace("1)", "0.2)"),
                   // @ts-ignore
-                  borderColor: tag.tag.color,
+                  borderColor: tag.color,
                 }}
               >
-                {tag.tag.tagName}
+                {tag.tagName}
               </div>
             ))}
           </div>
