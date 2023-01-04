f97b5f34f11bf84f1472d64db0289284a10d6e7f
ayabdi <abdulrahmanabdi98@gmail.com>
2023-01-04 22:31:37 +0300

    simplify schema

--- src/types/messages.schema.ts
+++ src/types/messages.schema.ts
@@ -1,20 +1,15 @@
+
 import { z } from "zod";
 
-export const NewTagSchema = z.object({
+export const CreateTagSchema = z.object({
     color: z.string(),
     tagName: z.string(),
-    type: z.literal("new")
+    tagId: z.string().optional(),
 })
-export const ExistingTagSchema = z.object({
-    tagId: z.string(),
-    type: z.literal("existing")
-})
-
 export const CreateMessageSchema = z.object({
     text: z.string(),
-    tags: z.array(NewTagSchema.or(ExistingTagSchema)).optional(),
+    tags: z.array(CreateTagSchema).optional(),
 })
 
-export type NewTag = z.infer<typeof NewTagSchema>;
-export type ExistingTag = z.infer<typeof ExistingTagSchema>;
+export type CreateTagSchema = z.infer<typeof CreateTagSchema>;
 export type CreateMessageSchema = z.infer<typeof CreateMessageSchema>;
