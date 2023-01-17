bf35c66aa594a43d04271b3f920ee2081a2586c2
ayabdi <abdulrahmanabdi98@gmail.com>
2023-01-17 15:02:08 +0300

    search options popover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const App: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen flex-col items-center justify-center bg-zinc-800">
-        <div className="fixed top-0 h-[55px] w-full border-b-[0.5px] border-slate-700 py-4">
+        <div className="fixed z-10 top-0 h-[55px] w-full border-b-[0.5px] border-slate-700 py-4">
           <SearchBar />
         </div>
         {session?.user ? <Feed /> : <Login />}
--- src/ui/components/SearchBar.tsx
+++ src/ui/components/SearchBar.tsx
@@ -11,11 +11,12 @@ import {
 } from "draft-js";
 import "draft-js/dist/Draft.css";
 import { MoonLoader } from "react-spinners";
+import { Popover } from "@headlessui/react";
 
 export const SearchBar = () => {
   const [tagsToFilter, setTagsToFilter] = useAtom(tagsToFilterAtom);
   const [allMessages, setAllMessages] = useAtom(allMessagesAtom);
-
+  const [showOptions, setShowOptions] = useState(false);
   const [editorState, setEditorState] = useState(() =>
     EditorState.createWithContent(emptyContentState)
   );
@@ -32,23 +33,6 @@ export const SearchBar = () => {
       },
     }
   );
-
-  const findSearchParams = (contentBlock: ContentBlock) => {
-    const text = contentBlock.getText();
-
-    const TAG_REGEX = /tag:\w+(,\w+)*/;
-    const BEFORE_REGEX = /before:\d{4}-\d{2}-\d{2}/;
-    const AFTER_REGEX = /after:\d{4}-\d{2}-\d{2}/;
-    const DURING_REGEX = /during:\d{4}-\d{2}-\d{2}/;
-
-    const params = [];
-    if (TAG_REGEX.test(text)) params.push(text.match(TAG_REGEX)?.[0]);
-    if (BEFORE_REGEX.test(text)) params.push(text.match(BEFORE_REGEX)?.[0]);
-    if (AFTER_REGEX.test(text)) params.push(text.match(AFTER_REGEX)?.[0]);
-    if (DURING_REGEX.test(text)) params.push(text.match(DURING_REGEX)?.[0]);
-
-    return params;
-  };
 
   const strategy = (
     contentBlock: ContentBlock,
@@ -81,6 +65,13 @@ export const SearchBar = () => {
     );
   };
 
+  const emptyState = () =>
+    setEditorState(
+      EditorState.moveFocusToEnd(
+        EditorState.push(editorState, emptyContentState, "insert-characters")
+      )
+    );
+  
   useEffect(() => {
     if (editorState) {
       const newEditorState = EditorState.set(editorState, {
@@ -97,13 +88,6 @@ export const SearchBar = () => {
     }, 500);
     return () => clearTimeout(timeout);
   }, [editorState]);
-
-  const emptyState = () =>
-    setEditorState(
-      EditorState.moveFocusToEnd(
-        EditorState.push(editorState, emptyContentState, "insert-characters")
-      )
-    );
 
   useEffect(() => {
     // empty search bar
@@ -137,8 +121,14 @@ export const SearchBar = () => {
 
   return (
     <div className="mx-auto -mt-0.5 flex w-1/2 min-w-[600px] max-w-[700px] rounded border-[0.1px] border-zinc-600 bg-[#36363B] px-2.5 py-0.5 text-white shadow">
+      <FilterOptions showOptions={showOptions} />
       <div className="flex-1">
-        <Editor editorState={editorState} onChange={handleEditorChange} />
+        <Editor
+          editorState={editorState}
+          onChange={handleEditorChange}
+          onFocus={() => setShowOptions(true)}
+          onBlur={() => setShowOptions(false)}
+        />
       </div>
       <MoonLoader
         color="#ffff"
@@ -157,8 +147,9 @@ export const SearchBar = () => {
         />
       )}
       <img
-        className="my-auto ml-1.5 h-5 cursor-pointer"
+        className="ml-auto inline h-6 cursor-pointer"
         src="/icons/filter.svg"
+        onClick={() => setShowOptions(!showOptions)}
       />
       <img
         className="my-auto ml-2 h-5 cursor-pointer bg-inherit"
@@ -166,6 +157,56 @@ export const SearchBar = () => {
       />
     </div>
   );
+};
+
+const FilterOptions = ({ showOptions }: { showOptions: boolean }) => {
+  return (
+    <Popover className="relative ">
+      <Popover.Panel
+        static={showOptions}
+        className="absolute -left-3  top-8 z-10 w-[50vw] min-w-[600px] max-w-[700px] origin-top-right "
+      >
+        <div className="w-full rounded-md  border-[0.5px] border-zinc-600 bg-[#36363B] py-3 text-zinc-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="px-3 py-1 font-semibold uppercase">
+            Search Options
+          </div>
+          <div className="px-3 py-1.5 hover:bg-zinc-700">
+            <span className="font-semibold">tags: </span>
+            <span className="ml-1 text-zinc-300">specific tags</span>
+          </div>
+          <div className="px-3 py-1.5 hover:bg-zinc-700">
+            <span className="font-semibold">during: </span>
+            <span className="ml-1 text-zinc-300">specific date</span>
+          </div>
+          <div className="px-3 py-1.5 hover:bg-zinc-700">
+            <span className="font-semibold">before: </span>
+            <span className="ml-1 text-zinc-300">before date</span>
+          </div>
+          <div className="px-3 py-1.5 hover:bg-zinc-700">
+            <span className="font-semibold">after: </span>
+            <span className="ml-1 text-zinc-300">after date</span>
+          </div>
+        </div>
+      </Popover.Panel>
+    </Popover>
+  );
+};
+
+const findSearchParams = (contentBlock: ContentBlock) => {
+  const text = contentBlock.getText();
+
+  const TAG_REGEX = /tag:\w+(,\w+)*/;
+  const BEFORE_REGEX = /before:\d{4}-\d{2}-\d{2}/;
+  const AFTER_REGEX = /after:\d{4}-\d{2}-\d{2}/;
+  const DURING_REGEX = /during:\d{4}-\d{2}-\d{2}/;
+
+  const params = [];
+  if (TAG_REGEX.test(text)) params.push(text.match(TAG_REGEX)?.[0]);
+  if (BEFORE_REGEX.test(text)) params.push(text.match(BEFORE_REGEX)?.[0]);
+  if (AFTER_REGEX.test(text)) params.push(text.match(AFTER_REGEX)?.[0]);
+  if (DURING_REGEX.test(text)) params.push(text.match(DURING_REGEX)?.[0]);
+
+  return params;
 };
 
 const emptyContentState = convertFromRaw({
