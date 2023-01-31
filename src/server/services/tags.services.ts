c92760b6dc404cfb78b20c622da58d7624a131e7
Abdulrahman Abdi <46926470+ayabdi@users.noreply.github.com>
2023-01-31 20:23:58 +0300

    return unique tags

--- src/server/services/tags.services.ts
+++ src/server/services/tags.services.ts
@@ -1,16 +1,24 @@
 import { prisma } from "@/server/db/client";
+import { Tag } from "@prisma/client";
 
 export const getTags = async (userId: string) => {
-    const result = await prisma.tagsOnMessages.findMany({
-        where: {
-            message: {
-                userId,
-            },
-        },
-        include: {
-            tag: true,
-        },
-    });
-    const tags = result.map((tag) => tag.tag);
-    return tags;
-}
+  const result = await prisma.tagsOnMessages.findMany({
+    where: {
+      message: {
+        userId,
+      },
+    },
+    include: {
+      tag: true,
+    },
+  });
+
+  const uniqueTags = result.reduce((acc: Tag[], tag) => {
+    if (!acc.find((t) => t.id === tag.tag.id)) {
+      acc.push(tag.tag);
+    }
+    return acc;
+  }, []);
+
+  return uniqueTags;
+};
