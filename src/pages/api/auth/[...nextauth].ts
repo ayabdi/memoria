a24a152394338cfea6b9d74c65b95afed737ede4
ayabdi <abdulrahmanabdi98@gmail.com>
2023-02-04 14:22:15 +0300

    create welcome message

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,8 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import { createMessage } from "@/server/services/messages.service.js";
+import { CreateMessageSchema } from "@/types/messages.schema.js";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -29,6 +31,29 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     })
   ],
+  events: {
+    createUser: async ({ user }) => {
+      // Create a welcome message for the user
+      const message: CreateMessageSchema = {
+        content: `### Welcome to Memoria!
+        ---
+        <img src="https://inspgr.id/app/uploads/2014/10/motion-radio-05.gif" width="200" height="200">
+        <br>
+        
+        Your simple to use 2nd brain 🧠🚀
+        
+        Simply jot down whatever you have in your head, add relevant tags, and hit send!
+        <br>
+        No need to spend your valuable time and energy having to organize stuff, simply filter your posts by tag, and voila!
+        
+        I'll be your personal smart assistant along the way, simply enter  **@chat**  in the message box below with anything you wish to ask me.`,
+        type: "markdown",
+        from: "Memoria Bot",
+        tags: [{ tagName: "Bot", color: "rgba(54, 162, 235, 1)" }],
+      }
+      await createMessage(message, user.id)
+    }
+  }
 };
 
 export default NextAuth(authOptions);
