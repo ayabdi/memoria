c01b5ead696a401d80c4f9cb372b0b64356681ef
ayabdi <abdulrahmanabdi98@gmail.com>
2023-02-06 21:54:56 +0300

    create, search, delete vectors + schemas

--- src/server/lib/pinecone/index.ts
+++ src/server/lib/pinecone/index.ts
@@ -1,25 +1,86 @@
-import { env } from '@/env/server.mjs';
-import got from 'got';
+import { env } from "@/env/server.mjs";
+import { ServerMessageType } from "@/types/messages.schema";
+import got, { HTTPError } from "got";
+import {
+  PineconeSaveVector,
+  PineconeVectorSearch,
+  PineconeVectorSearchFilter,
+  PineconeVectorSearchResponse,
+  PineconeVectorSearchResponseSchema,
+} from "./schema";
 
-
-const apiKey = env.PINECONE_API_KEY
+const apiKey = env.PINECONE_API_KEY;
 
 const pinecone = got.extend({
-    prefixUrl: 'https://squad-784c8ba.svc.us-east1-gcp.pinecone.io/',
-    headers: {
-        'Content-Type': 'application/json',
-        'API-Key': apiKey,
-    },  
+  prefixUrl: "https://memoria-784c8ba.svc.us-east1-gcp.pinecone.io/",
+  headers: {
+    "Content-Type": "application/json",
+    "API-Key": apiKey,
+  },
 });
+const catchError = (e: any) => {
+  if (e instanceof HTTPError) {
+    throw new Error(e.message);
+  }
+  throw e;
+};
 
-export const addVector = async (id: string, values ) => {
-    const response = await pinecone.post('vectors/upsert', {
-        json: {
-            vector,
-            id: userId,
+export const saveVector = async (
+  message: ServerMessageType,
+  values: number[],
+  namespace: "conversation" | "message"
+) => {
+  const body: PineconeSaveVector = {
+    namespace,
+    vectors: [
+      {
+        id: message.id,
+        values: values,
+        namespace,
+        metadata: {
+          content: message.content,
+          type: message.type,
+          from: message.from,
+          userId: message.userId,
+          createdAt: message.createdAt,
+          updatedAt: message.updatedAt,
+          tags: message.tags.map((t) => t.tag.tagName),
         },
-    });
-    return response;
-}
+      },
+    ],
+  };
+  const response = await pinecone.post("vectors/upsert", { json: body }).json().catch(catchError);
+  return response;
+};
 
+export const searchVectors = async (
+  values: number[],
+  namespace: "conversation" | "message",
+  limit: number = 10,
+  filter?: PineconeVectorSearchFilter
+) => {
+  const body: PineconeVectorSearch = {
+    vector: values,
+    filter,
+    topK: limit,
+    namespace,
+    includeMetadata: true,
+  };
 
+  const response = await pinecone
+    .post("query", { json: body })
+    .json<PineconeVectorSearchResponse>()
+    .catch(catchError);
+  return PineconeVectorSearchResponseSchema.parse(response);
+};
+
+export const deleteVector = async (ids: string, namespace: string) => {
+    console.log(ids, namespace)
+  const response = await pinecone
+    .delete(`vectors/delete?namespace=${namespace}&ids=${ids}`)
+    .json()
+    .catch(catchError);
+  return response;
+};
+
+// do edit vector
--- src/server/lib/pinecone/schema.ts
+++ src/server/lib/pinecone/schema.ts
@@ -1,3 +1,57 @@
+import { z } from "zod";
 
+export const PineconeMetadataSchema = z.object({
+  content: z.string(),
+  type: z.string(),
+  from: z.string(),
+  userId: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  tags: z.array(z.string()),
+});
 
+export const PineconeVectorSchema = z.object({
+  id: z.string(),
+  values: z.array(z.number()),
+  namespace: z.enum(["conversation", "message"]),
+  metadata: PineconeMetadataSchema,
+});
+export const PineConeSearchFilterSchema = z.object({
+  content: z.string().optional(),
+  type: z.string().optional(),
+  from: z.string().optional(),
+  userId: z.string().optional(),
+  createdAt: z.date().optional(),
+});
 
+export const PineconeVectorSearchSchema = z.object({
+  vector: z.array(z.number()),
+  filter: PineConeSearchFilterSchema.optional(),
+  topK: z.number(),
+  namespace: z.enum(["conversation", "message"]),
+  includeMetadata: z.boolean().optional(),
+  includeVectors: z.boolean().optional(),
+});
+
+export const PineconeSaveVectorSchema = z.object({
+  vectors: z.array(PineconeVectorSchema),
+  namespace: z.string(),
+});
+
+export const PineconeVectorSearchResponseSchema = z.object({
+  matches: z.array(
+    z.object({
+      id: z.string(),
+      score: z.number(),
+      metadata: PineconeMetadataSchema,
+    })
+  ),
+  namespace: z.string(),
+});
+
+export type PineconeVector = z.infer<typeof PineconeVectorSchema>;
+export type PineconeVectorSearch = z.infer<typeof PineconeVectorSearchSchema>;
+export type PineconeVectorSearchResponse = z.infer<typeof PineconeVectorSearchResponseSchema>;
+export type PineconeSaveVector = z.infer<typeof PineconeSaveVectorSchema>;
+export type PineconeMetadata = z.infer<typeof PineconeMetadataSchema>;
+export type PineconeVectorSearchFilter = z.infer<typeof PineConeSearchFilterSchema>;
