0e08061399348b7a03ec5337aaf8898e6f97ec9c
ayabdi <abdulrahmanabdi98@gmail.com>
2023-02-07 20:00:11 +0300

    pinecone integration complete

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,7 +2,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export const Login: React.FC = () => {
     const { data: sessionData } = useSession();
-  
     return (
       <div className="flex flex-col items-center justify-center gap-4">
         <p className="text-center text-2xl text-white">
