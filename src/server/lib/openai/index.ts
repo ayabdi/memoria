e241fb6995c250ebfc5f5d3e93a21112662b181b
ayabdi <abdulrahmanabdi98@gmail.com>
2023-02-21 21:57:10 +0300

    throw errors

--- src/server/lib/openai/index.ts
+++ src/server/lib/openai/index.ts
@@ -3,57 +3,57 @@ import { env } from "../../../env/server.mjs";
 import { User } from "@prisma/client";
 
 const config = new Configuration({
-    apiKey: env.OPENAI_API_KEY
+  apiKey: env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(config);
 
-
 export async function gptEmbedding(input: string): Promise<number[]> {
+  try {
     const response = await openai.createEmbedding({
-        model: 'text-embedding-ada-002',
-        input
+      model: "text-embedding-ada-002",
+      input,
     });
     if (!response.data.data || response.data.data.length === 0)
-        throw new Error('No embedding found');
+      throw new Error("No embedding found");
 
-    const vector = response.data.data[0]?.embedding
-    if (!vector)
-        throw new Error('No vector found');
+    const vector = response.data.data[0]?.embedding;
 
+    if (!vector) throw new Error("No vector found");
     return vector;
+  } catch (error) {
+    throw new Error(`Error communicating with OpenAI: ${error}`);
+  }
 }
 
-
 export async function gptCompletion(prompt: string, stop: string[]): Promise<string> {
-    const max_retry = 5;
-    let retry = 0;
-    prompt = prompt.replace(/[^\x00-\x7F]/g, "");
-    while (true) {
-        try {
-            const response = await openai.createCompletion({
-                model: 'text-davinci-003',
-                prompt,
-                temperature: 0.5,
-                max_tokens: 400,
-                
-                frequency_penalty: 0.0,
-                presence_penalty: 0.0,
-               
-            });
-            if (!response?.data?.choices || response?.data?.choices.length === 0) {
-                return 'GPT3 error: No response';
-            }
-            let text = response.data.choices[0]?.text?.trim() || '';
-            text = text.replace(/[\r\n]+/g, '\n');
-            text = text.replace(/[\t ]+/g, ' ');
-            return text;
-        } catch (error) {
-            retry += 1;
-            if (retry >= max_retry) {
-                return `GPT3 error: ${error}`;
-            }
-            console.log(`Error communicating with OpenAI: ${error}`);
-            await new Promise(resolve => setTimeout(resolve, 1000));
-        }
+  const max_retry = 5;
+  let retry = 0;
+  prompt = prompt.replace(/[^\x00-\x7F]/g, "");
+  while (true) {
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.5,
+        max_tokens: 400,
+
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      });
+      if (!response?.data?.choices || response?.data?.choices.length === 0) {
+        return "GPT3 error: No response";
+      }
+      let text = response.data.choices[0]?.text?.trim() || "";
+      text = text.replace(/[\r\n]+/g, "\n");
+      text = text.replace(/[\t ]+/g, " ");
+      return text;
+    } catch (error) {
+      retry += 1;
+      if (retry >= max_retry) {
+        throw new Error(`GPT3 error: ${error}`);
+      }
+      console.log(`Error communicating with OpenAI: ${error}`);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
+  }
 }
